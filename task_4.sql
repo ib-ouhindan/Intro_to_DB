5a417d8b1e582b78e216a8c157e73330af6e9e05
Ouhindan <101867910+ib-ouhindan@users.noreply.github.com>
2024-07-28 21:02:24 +0100

    Create task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,13 @@
+-- Print the full description of the books table from the alx_book_store database
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+    EXTRA 
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'books';
